c88d463822a0a1e914f5848422ddd39ab6b8dce9
Sumit Dhiman <76207887+lucifer1708@users.noreply.github.com>
2022-02-19 02:30:17 +0530

    Update settings.py

--- cryptic/settings.py
+++ cryptic/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     
     # third party
     'crispy_forms',
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
